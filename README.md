e8ed8e310b604d209732b9da333b09b400c983a0
Kris Hatch-McCarter <userkris169@yahoo.com>
2020-08-04 14:24:27 -0700

    IncorporatePeerReviewFeedbackAddReadMe

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # treehouse-FSJSTechDegree-Project01
 My Treehouse FSJS Techdegree Project 01 - A Random Quote Generator
+
+An app that displays random famous quotes each time a button is clicked.
--- js/script.js
+++ js/script.js
@@ -3,34 +3,23 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-//Create variable named 'quotes' 
-let quotes = [];
+//Create variable named 'quotes' with five quote objects
+let quotes = [
+{ quote: 'It\'s not about what it is, it’s about what it can become',
+  source: 'The Lorax',
+  citation: 'Dr. Seuss The Lorax',
+  year: 1971},
+{ quote: '[Kids] don\'t remember what you try to teach them. They remember what you are.',
+  source: 'Jim Henson'},
+{ quote: 'Love isn\'t a state of perfect caring. It is an active noun like struggle. To love someone is to strive to accept that person exactly the way he or she is, right here and now.',
+  source: 'Fred Rogers'},
+{ quote: 'There are no bad people, there are people with insufficient information to make appropriate decisions',
+  source: 'Jacque Fresco'},
+{ quote: 'There\'s nothing wrong with talking to yourself.',
+  source: 'Bubbles',
+  tags: 'PowerPuff Girls'},    
+];
 
-//Add five empty, comma separated objects to the array. 
-for (i = 0; i < 5; i ++) {
-  quotes.push({})
-   }
-//Add data to quote objects. quote string for actual quote and source string for the person or character who said it
-  quotes[0].quote = 'It\'s not about what it is, it’s about what it can become';
-  quotes[0].source = 'The Lorax';
-  quotes[1].quote = '[Kids] don\'t remember what you try to teach them. They remember what you are.';
-  quotes[1].source = 'Jim Henson';
-  quotes[2].quote = 'Love isn\'t a state of perfect caring. It is an active noun like struggle. To love someone is to strive to accept that person exactly the way he or she is, right here and now.';
-  quotes[2].source = 'Fred Rogers';
-  quotes[3].quote = 'There are no bad people, there are people with insufficient information to make appropriate decisions';
-  quotes[3].source = 'Jacque Fresco';
-  quotes[4].quote = 'There\'s nothing wrong with talking to yourself.';
-  quotes[4].source = 'Bubbles';
-
-  //Add a citation and year
-  quotes[0].citation = 'Dr. Seuss The Lorax';
-  quotes[0].year = 1971;
-
-  //Add additional quote object property [For exceeds grade]
-   quotes[4].tags = 'PowerPuff Girls';
-
-  // console.log(quotes[0]);
- 
 //Create getRandomQuote function: create a variable to store a random number representing the index of the quotes array, return a random quote object using the random number variable and bracket notation on the quotes array.
 
 function getRandomQuote (){
@@ -38,15 +27,13 @@ function getRandomQuote (){
   return quotes[randomNumber];
 }
 
-//console.log(getRandomQuote());
-
 // Create printQuote function
 
 function printQuote(){
-//console.log('printing quote');
+
 //Create a variable to store a random quote object from the getRandomQuote() function
 let randomQuote = getRandomQuote();
-//console.log(randomQuote['source']);
+
 //Create another variable to store the HTML string. 
 let html = `<p class="quote"> ${randomQuote.quote} </p>
 <p class="source"> ${randomQuote.source}
@@ -77,9 +64,6 @@ function randomRGB(value){
 }
 
 document.body.style.backgroundColor = randomRGB(randomValue);
-
-//function to return the full HTML string displaying a random quote.
-return html;
 
 }
 
